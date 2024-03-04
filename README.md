fb5ad268a7b8a2aaef6e8986d505127b3a845674
vanshika9 <vanshikagupta70071@gmail.com>
2024-03-05 03:07:28 +0530

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# React Practice 🥹
+# React Practice 
--- app.js
+++ app.js
@@ -1,25 +1,29 @@
+// import React from '../node_modules/react/app.js';
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
 // const h1 = React.createElement("h1", {id:"xyz"}, "Hello World From React!")
 // console.log(h1)//object
 // const r1 = ReactDOM.createRoot(document.getElementById("rot"))
 // r1.render(h1);
 
-const h2=React.createElement("div", {id:"parent"},
+const parent=React.createElement("div", {id:"parent"},
 [
 React.createElement("div", {id:"child"},
 [React.createElement("h1", {}, "I am a h1 tag"),
 React.createElement("h2", {}, "I am a h2 tag"),
-React.createElement("h3", {}, "I am a h3 tag")]
+React.createElement("h3", {}, "I am a h4 tag")]
 ),
 
 React.createElement("div", {id:"child2"},
 [React.createElement("h1", {}, "I am a h1 tag"),
 React.createElement("h2", {}, "I am a h2 tag"),
-React.createElement("h3", {}, "I am a h3 tag")]
+React.createElement("h3", {}, "I am a h5 tag")]
 )
 ]
 )
 
-const r2=ReactDOM.createRoot(document.getElementById("rot"))
+const root=ReactDOM.createRoot(document.getElementById("root"))
 
-r2.render(h2)
-console.log(h2)
+root.render(parent)
+console.log(parent)
